5423c65396427acfbbe89f0af8cbedf479bcfd76
ToodleBit <45330091+ToodleBit@users.noreply.github.com>
2018-11-25 20:15:26 +0000

    Update toodlebit.ts

--- toodlebit.ts
+++ toodlebit.ts
@@ -258,7 +258,7 @@ namespace ToodleBit {
     //% weight=4
 	//% advanced=true
     //% blockId=toodlebit_forwardseconds block="forward:(ms) %pause"
-    export function forwardSeconds(ms: int): void {
+    export function forwardSeconds(ms: number): void {
         // Add code here
 			pins.servoSetPulse(pin_left_wheel, 1300)
 			pins.servoSetPulse(pin_right_wheel, 1700)
