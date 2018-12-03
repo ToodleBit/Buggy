31be6f579e725288974f93e7e4bd7821d82bfcce
ToodleBit <45330091+ToodleBit@users.noreply.github.com>
2018-12-03 17:18:52 +0000

    Update toodlebit.ts

--- toodlebit.ts
+++ toodlebit.ts
@@ -226,10 +226,10 @@ namespace ToodleBit {
                 pins.servoSetPulse(pin_left_wheel, 1700)
                 break
             case -3:
-				pins.servoSetPulse(pin_left_wheel, 1670)
+				pins.servoSetPulse(pin_left_wheel, 1630)
                 break
             case -2:
-				pins.servoSetPulse(pin_left_wheel, 1630)
+				pins.servoSetPulse(pin_left_wheel, 1670)
 				break
 			case -1:
                 pins.servoSetPulse(pin_left_wheel, 1590)
@@ -241,10 +241,10 @@ namespace ToodleBit {
                 pins.servoSetPulse(pin_left_wheel, 1410)
                 break
 			case 2:
-                pins.servoSetPulse(pin_left_wheel, 1360)
+                pins.servoSetPulse(pin_left_wheel, 1390)
                 break
 			case 3:
-                pins.servoSetPulse(pin_left_wheel, 1320)
+                pins.servoSetPulse(pin_left_wheel, 1370)
                 break
 			case 4:
                 pins.servoSetPulse(pin_left_wheel, 1300)
@@ -264,7 +264,7 @@ namespace ToodleBit {
                 pins.servoSetPulse(pin_right_wheel, 1410)
                 break
             case -3:
-				pins.servoSetPulse(pin_right_wheel, 1360)
+				pins.servoSetPulse(pin_right_wheel, 1390)
                 break
             case -2:
 				pins.servoSetPulse(pin_right_wheel, 1320)
@@ -276,10 +276,10 @@ namespace ToodleBit {
                 pins.digitalWritePin(digital_pin_right_wheel, 0) //stop
                 break
 			case 1:
-                pins.servoSetPulse(pin_right_wheel, 1700)
+                pins.servoSetPulse(pin_right_wheel, 1590)
                 break
 			case 2:
-                pins.servoSetPulse(pin_right_wheel, 1670)
+                pins.servoSetPulse(pin_right_wheel, 1610)
                 break
 			case 3:
                 pins.servoSetPulse(pin_right_wheel, 1630)
