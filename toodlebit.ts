d46c95dcf47cd0e43f6ec009251c364d46f760b3
ToodleBit <45330091+ToodleBit@users.noreply.github.com>
2018-12-03 15:51:25 +0000

    Update toodlebit.ts

--- toodlebit.ts
+++ toodlebit.ts
@@ -2,10 +2,7 @@
  * Use this file to define custom functions and blocks.
  * Read more at https://makecode.microbit.org/blocks/custom
  */
- enum Direction {
-    forwards,
-    backwards
-}
+
 
 /**
  * Custom blocks
